14badb1d896c889a9eb48c40448dea8e71314284
unknown <ex-zhouliang005@paxjk.com>
2020-04-13 18:02:33 +0800

    添加全局config配置文件

---
+++ config/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const config = require('../config')
+const baseLoader = require('./loader')
+let basePath = function(dirPath){
+  return path.resolve(__dirname,'../',dirPath)
+}
+module.exports = {
+  entry:basePath(config.entry || 'src/main.js'),
+  output:{
+    filename: config.output.filename || '[name].bundle.js',
+    path: basePath(config.output.path || 'dist')
+  },
+  // module:{
+  //   rules:config.rules?[...baseLoader,...config.rules]:[...baseLoader],  
+  // }
+}
